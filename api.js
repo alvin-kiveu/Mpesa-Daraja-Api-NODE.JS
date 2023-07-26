fba567fa8f5ad17816a010a1690b0c56c0b4afda
Alvin Kiveu <67068458+alvin-kiveu@users.noreply.github.com>
2023-07-26 09:23:39 +0300

    adding new data

---
+++ api.js
@@ -0,0 +1,154 @@
+const express = require('express');
+const router = express.Router();
+const axios = require("axios");
+const fs = require("fs");
+const moment = require("moment");
+
+
+// Sample API route
+router.get('/api/home', (req, res) => {
+  res.json({ message: 'This is a sample API route.' });
+  console.log("This is a sample API route.");
+});
+
+router.get("/api/access_token", (req, res) => {
+  getAccessToken()
+    .then((accessToken) => {
+      res.json({ message: "😀 Your access token is " + accessToken });
+    })
+    .catch(console.log);
+});
+
+async function getAccessToken() {
+  const consumer_key = "B2JNQ0jXldRaVNrSNf7PIhZCw8u6DUAE"; // REPLACE IT WITH YOUR CONSUMER KEY
+  const consumer_secret = "vmDhsm56BY5Afqva"; // REPLACE IT WITH YOUR CONSUMER SECRET
+  const url =
+    "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
+
+  const auth =
+    "Basic " +
+    new Buffer.from(consumer_key + ":" + consumer_secret).toString("base64");
+
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        Authorization: auth,
+      },
+    });
+    const accessToken = response.data.access_token;
+    return accessToken;
+  } catch (error) {
+    throw error;
+  }
+}
+
+router.post('/api/stkpush', (req, res) => {
+  let phoneNumber = req.body.phone;
+  const accountNumber = req.body.accountNumber;
+  const amount = req.body.amount;
+
+  if (phoneNumber.startsWith("0")) {
+    phoneNumber = "254" + phoneNumber.slice(1);
+  }
+
+
+  //ECHO  THE DATA THAT WE RECEIVED FROM THE CLIENT
+  // console.log("Phone Number:", phoneNumber);
+  // console.log("Account Number:", accountNumber);
+  // console.log("Amount:", amount);
+  // res.json({ message: 'This is a sample API route.' });
+
+  getAccessToken()
+    .then((accessToken) => {
+      const url =
+        "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest";
+      const auth = "Bearer " + accessToken;
+      const timestamp = moment().format("YYYYMMDDHHmmss");
+      const password = new Buffer.from(
+        "174379" +
+        "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919" +
+        timestamp
+      ).toString("base64");
+
+      axios
+        .post(
+          url,
+          {
+            BusinessShortCode: "174379",
+            Password: password,
+            Timestamp: timestamp,
+            TransactionType: "CustomerPayBillOnline",
+            Amount: amount,
+            PartyA: phoneNumber,
+            PartyB: "174379",
+            PhoneNumber: phoneNumber,
+            CallBackURL: "https://249e-105-60-226-239.ngrok-free.app/api/callback",
+            AccountReference: accountNumber,
+            TransactionDesc: "Mpesa Daraja API stk push test",
+          },
+          {
+            headers: {
+              Authorization: auth,
+            },
+          }
+        )
+        .then((response) => {
+          // res.send("😀 Request is successful done ✔✔. Please enter mpesa pin to complete the transaction");
+          //SEND BACK A JSON RESPONSE TO THE CLIENT
+          console.log(response.data);
+          res.status(200).json({
+            msg: "Request is successful done ✔✔. Please enter mpesa pin to complete the transaction",
+            status: true,
+          });
+
+        })
+        .catch((error) => {
+          console.log(error);
+          //res.status(500).send("❌ Request failed");
+          console.log(error);
+          res.status(500).json({
+            msg: "Request failed",
+            status: false,
+          });
+        });
+    })
+    .catch(console.log);
+});
+
+
+
+router.post("/api/callback", (req, res) => {
+  console.log("STK PUSH CALLBACK");
+  const merchantRequestID = req.body.Body.stkCallback.MerchantRequestID;
+  const checkoutRequestID = req.body.Body.stkCallback.CheckoutRequestID;
+  const resultCode = req.body.Body.stkCallback.ResultCode;
+  const resultDesc = req.body.Body.stkCallback.ResultDesc;
+  const callbackMetadata = req.body.Body.stkCallback.CallbackMetadata;
+  const amount = callbackMetadata.Item[0].Value;
+  const mpesaReceiptNumber = callbackMetadata.Item[1].Value;
+  const transactionDate = callbackMetadata.Item[3].Value;
+  const phoneNumber = callbackMetadata.Item[4].Value;
+
+  console.log("MerchantRequestID:", merchantRequestID);
+  console.log("CheckoutRequestID:", checkoutRequestID);
+  console.log("ResultCode:", resultCode);
+  console.log("ResultDesc:", resultDesc);
+  
+  console.log("Amount:", amount);
+  console.log("MpesaReceiptNumber:", mpesaReceiptNumber);
+  console.log("TransactionDate:", transactionDate);
+  console.log("PhoneNumber:", phoneNumber);
+
+  var json = JSON.stringify(req.body);
+  fs.writeFile("stkcallback.json", json, "utf8", function (err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("STK PUSH CALLBACK STORED SUCCESSFULLY");
+  });
+});
+
+
+// Other API routes go here...
+
+module.exports = router;
--- app.js
+++ app.js
@@ -2,13 +2,44 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const bodyParser = require("body-parser");
-const request = require("request");
+const axios = require("axios"); // Import 'axios' instead of 'request'
 const moment = require("moment");
+const apiRouter = require('./api');
+const cors = require("cors");
 
-const port = 3000;
+
+const port = 5000;
 const hostname = "localhost";
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use('/', apiRouter);
+
 const server = http.createServer(app);
+
+// ACCESS TOKEN FUNCTION - Updated to use 'axios'
+async function getAccessToken() {
+  const consumer_key = ""; // REPLACE IT WITH YOUR CONSUMER KEY
+  const consumer_secret = ""; // REPLACE IT WITH YOUR CONSUMER SECRET
+  const url =
+    "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
+
+  const auth =
+    "Basic " +
+    new Buffer.from(consumer_key + ":" + consumer_secret).toString("base64");
+
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        Authorization: auth,
+      },
+    });
+    const accessToken = response.data.access_token;
+    return accessToken;
+  } catch (error) {
+    throw error;
+  }
+}
 
 app.get("/", (req, res) => {
   res.send("MPESA DARAJA API WITH NODE JS BY UMESKIA SOFTWARES");
@@ -25,29 +56,24 @@ app.get("/access_token", (req, res) => {
     .catch(console.log);
 });
 
-
 //MPESA STK PUSH ROUTE
 app.get("/stkpush", (req, res) => {
   getAccessToken()
     .then((accessToken) => {
       const url =
-          "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest",
-        auth = "Bearer " + accessToken;
-      var timestamp = moment().format("YYYYMMDDHHmmss");
+        "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest";
+      const auth = "Bearer " + accessToken;
+      const timestamp = moment().format("YYYYMMDDHHmmss");
       const password = new Buffer.from(
         "174379" +
           "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919" +
           timestamp
       ).toString("base64");
 
-      request(
-        {
-          url: url,
-          method: "POST",
-          headers: {
-            Authorization: auth,
-          },
-          json: {
+      axios
+        .post(
+          url,
+          {
             BusinessShortCode: "174379",
             Password: password,
             Timestamp: timestamp,
@@ -60,53 +86,57 @@ app.get("/stkpush", (req, res) => {
             AccountReference: "UMESKIA PAY",
             TransactionDesc: "Mpesa Daraja API stk push test",
           },
-        },
-        function (error, response, body) {
-          if (error) {
-            console.log(error);
-          } else {
-            res.send("😀 Request is successful done ✔✔. Please enter mpesa pin to complete the transaction");
+          {
+            headers: {
+              Authorization: auth,
+            },
           }
-        }
-      );
+        )
+        .then((response) => {
+          res.send("😀 Request is successful done ✔✔. Please enter mpesa pin to complete the transaction");
+        })
+        .catch((error) => {
+          console.log(error);
+          res.status(500).send("❌ Request failed");
+        });
     })
     .catch(console.log);
 });
 
-
-//REGISTER URL FOT C2B
+// REGISTER URL FOR C2B
 app.get("/registerurl", (req, resp) => {
   getAccessToken()
     .then((accessToken) => {
-      let url = "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl";
-      let auth = "Bearer " + accessToken;
-      request(
-        {
-          url: url,
-          method: "POST",
-          headers: {
-            Authorization: auth,
-          },
-          json: {
+      const url = "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl";
+      const auth = "Bearer " + accessToken;
+      axios
+        .post(
+          url,
+          {
             ShortCode: "174379",
             ResponseType: "Complete",
             ConfirmationURL: "http://example.com/confirmation",
             ValidationURL: "http://example.com/validation",
           },
-        },
-        function (error, response, body) {
-          if (error) {
-            console.log(error);
+          {
+            headers: {
+              Authorization: auth,
+            },
           }
-          resp.status(200).json(body);
-        }
-      );
+        )
+        .then((response) => {
+          resp.status(200).json(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+          resp.status(500).send("❌ Request failed");
+        });
     })
     .catch(console.log);
 });
 
 app.get("/confirmation", (req, res) => {
-  console.log("All transaction  will be send to this url");
+  console.log("All transaction will be sent to this URL");
   console.log(req.body);
 });
 
@@ -115,89 +145,45 @@ app.get("/validation", (req, resp) => {
   console.log(req.body);
 });
 
-//B2C ROUTE OR AUTO WITHDRAWAL
+// B2C ROUTE OR AUTO WITHDRAWAL
 app.get("/b2curlrequest", (req, res) => {
   getAccessToken()
-  .then((accessToken) => {
-    const securityCredential = "N3Lx/hisedzPLxhDMDx80IcioaSO7eaFuMC52Uts4ixvQ/Fhg5LFVWJ3FhamKur/bmbFDHiUJ2KwqVeOlSClDK4nCbRIfrqJ+jQZsWqrXcMd0o3B2ehRIBxExNL9rqouKUKuYyKtTEEKggWPgg81oPhxQ8qTSDMROLoDhiVCKR6y77lnHZ0NU83KRU4xNPy0hRcGsITxzRWPz3Ag+qu/j7SVQ0s3FM5KqHdN2UnqJjX7c0rHhGZGsNuqqQFnoHrshp34ac/u/bWmrApUwL3sdP7rOrb0nWasP7wRSCP6mAmWAJ43qWeeocqrz68TlPDIlkPYAT5d9QlHJbHHKsa1NA==";
-    const url =
-        "https://sandbox.safaricom.co.ke/mpesa/b2c/v1/paymentrequest",
-      auth = "Bearer " + accessToken;
-    request(
-      {
-        url: url,
-
-        method: "POST",
-
-        headers: {
-          Authorization: auth,
-        },
-
-        json: {
-          InitiatorName: "testapi",
-
-          SecurityCredential: securityCredential,
-
-          CommandID: "PromotionPayment",
-
-          Amount: "1",
-
-          PartyA: "600996",
-
-          PartyB: "254768168060",
-
-          Remarks: "Withdrawal",
-
-          QueueTimeOutURL:
-            "https://mydomain.com/b2c/queue",
-
-          ResultURL:
-            "https://mydomain.com/b2c/result",
-
-          Occasion: "Withdrawal",
-        },
-      },
-      function (error, response, body) {
-        if (error) {
+    .then((accessToken) => {
+      const securityCredential =
+        "N3Lx/hisedzPLxhDMDx80IcioaSO7eaFuMC52Uts4ixvQ/Fhg5LFVWJ3FhamKur/bmbFDHiUJ2KwqVeOlSClDK4nCbRIfrqJ+jQZsWqrXcMd0o3B2ehRIBxExNL9rqouKUKuYyKtTEEKggWPgg81oPhxQ8qTSDMROLoDhiVCKR6y77lnHZ0NU83KRU4xNPy0hRcGsITxzRWPz3Ag+qu/j7SVQ0s3FM5KqHdN2UnqJjX7c0rHhGZGsNuqqQFnoHrshp34ac/u/bWmrApUwL3sdP7rOrb0nWasP7wRSCP6mAmWAJ43qWeeocqrz68TlPDIlkPYAT5d9QlHJbHHKsa1NA==";
+      const url = "https://sandbox.safaricom.co.ke/mpesa/b2c/v1/paymentrequest";
+      const auth = "Bearer " + accessToken;
+      axios
+        .post(
+          url,
+          {
+            InitiatorName: "testapi",
+            SecurityCredential: securityCredential,
+            CommandID: "PromotionPayment",
+            Amount: "1",
+            PartyA: "600996",
+            PartyB: "254768168060",
+            Remarks: "Withdrawal",
+            QueueTimeOutURL: "https://mydomain.com/b2c/queue",
+            ResultURL: "https://mydomain.com/b2c/result",
+            Occasion: "Withdrawal",
+          },
+          {
+            headers: {
+              Authorization: auth,
+            },
+          }
+        )
+        .then((response) => {
+          res.status(200).json(response.data);
+        })
+        .catch((error) => {
           console.log(error);
-        }
-        res.status(200).json(body);
-      }
-      );
+          res.status(500).send("❌ Request failed");
+        });
     })
-      .catch(console.log);
-});   
-      
-// ACCESS TOKEN FUNCTION
-function getAccessToken() {
-  const consumer_key = "";// REPLACE IT WITH YOUR CONSUMER KEY
-  const consumer_secret = ""; // REPLACE IT WITH YOUR CONSUMER SECRET
-  const url =
-    "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
-  const auth =
-    "Basic " +
-    new Buffer.from(consumer_key + ":" + consumer_secret).toString("base64");
-  return new Promise((response, reject) => {
-    request(
-      {
-        url: url,
-        headers: {
-          Authorization: auth,
-        },
-      },
-      function (error, res, body) {
-        var jsonBody = JSON.parse(body);
-        if (error) {
-          reject(error);
-        } else {
-          const accessToken = jsonBody.access_token;
-          response(accessToken);
-        }
-      }
-    );
-  });
-}
-
+    .catch(console.log);
+});
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
